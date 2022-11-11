898bdb8e811558b59854c451731884d6df5e9b7f
marcusWittho <mcswitthoeft@gmail.com>
2022-11-11 13:19:27 -0300

    feat: Adição das classes PostDto e UserDto

---
+++ src/main/java/com/blog/api/dto/PostDto.java
@@ -0,0 +1,61 @@
+package com.blog.api.dto;
+
+import com.blog.api.model.User;
+
+public class PostDto {
+
+  private String title;
+  private String image;
+  private String description;
+  private String comments;
+  private String postDate;
+  private User user;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getComments() {
+    return comments;
+  }
+
+  public void setComments(String comments) {
+    this.comments = comments;
+  }
+
+  public String getPostDate() {
+    return postDate;
+  }
+
+  public void setPostDate(String postDate) {
+    this.postDate = postDate;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+}
